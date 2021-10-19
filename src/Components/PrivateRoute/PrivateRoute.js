5eab1eaa7322090b6578b7daa63c042e7d34de90
reyad98 <alexmarfi2233@gmail.com>
2021-10-19 21:22:34 +0600

    rer

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router';
 import useFirebase from '../../hooks/usefirebase';
 
 const PrivateRoute = ({ children, ...rest }) => {
